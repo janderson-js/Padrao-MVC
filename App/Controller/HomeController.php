a0b512c037589e4f2cc248f7ca01a814a13c2682
Janderson Souza <jandersonsouzagit@gmail.com>
2021-03-01 21:24:28 -0300

    Exibindo postagens

--- App/Controller/HomeController.php
+++ App/Controller/HomeController.php
@@ -5,8 +5,20 @@
         public function index()
         {
            try{
-                $teste = Postagem::selecionaTodos();
-                var_dump($teste);
+
+               $colectionPostagem = Postagem::selecionaTodos();
+
+                $loader = new \Twig\Loader\FilesystemLoader("App/View");
+                $twig = new \Twig\Environment($loader);
+                $template = $twig->load('home.html');
+                
+                $parametros = array();
+                $parametros['postagens'] = $colectionPostagem;
+
+                $conteudo = $template->render($parametros);
+
+                echo $conteudo;
+
            }catch(Exception $e){
                echo $e->getMessage();
            }
